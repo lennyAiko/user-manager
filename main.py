2edef5b485b5e28d9c7bf32e52ba0c8c9b343437
Lennox Charles <lennyaiko17@gmail.com>
2023-01-20 18:40:11 +0100

    now able to register users

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from db import db
+from models import User
 
 app = FastAPI()
 
@@ -10,3 +11,9 @@ async def root():
 @app.get("/api/v1/users")
 async def fetch_users():
     return db
+
+# user(a variable to hold the entity): User(the entity received)
+@app.post("/api/v1/users")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
