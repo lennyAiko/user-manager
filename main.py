0f4108a72afc09c34e61473e8bd8dc13d33b65a5
Lennox Charles <lennyaiko17@gmail.com>
2023-01-20 22:35:15 +0100

    can now update user

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from db import db
-from models import User
+from models import User, UserUpdateRequest
 from uuid import UUID
 
 app = FastAPI()
@@ -29,4 +29,23 @@ async def delete_user(user_id: UUID):
     raise HTTPException(
         status_code=404,
         detail=f"user with id: {user_id} does not exist."
+    )
+
+# getting the id and then sending a body/payload
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists"
     )
